c2e872fd12c0c06d6c7526a40ecd8dbae3d42246
Deepjyoti Barman <deep.barman30@gmail.com>
2022-03-14 18:15:37 +0530

    Add a script to add ann search data

---
+++ backend/search/annSearch.js
@@ -0,0 +1,46 @@
+async function handleRequest() {
+    const requestBody = JSON.parse(context.request.body)
+
+    if (requestBody.type.toLowerCase() == "text") return {}
+
+    const vectoredValue = await getVectorForData(requestBody.value);
+
+    return {
+        envs: {
+            ...envs,
+            path: "/app-store-data/_knn_search"
+        },
+        esBody: {
+            knn: {
+                field: "name_vector",
+                query_vector: vectoredValue,
+                k: 10,
+                num_candidates: 500
+            },
+            fields: ["name_vector", "desc_vector"],
+            _source: {
+                "exclude": ["name_vector", "desc_vector"]
+            }
+        }
+    }
+}
+
+
+async function getVectorForData(data) {
+    const url = `${context.envs.bertURL}/encode`;
+
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            id: 1,
+            texts: [data],
+            is_tokenized: false
+        })
+    });
+
+    const jsonResponse = JSON.parse(response);
+    return jsonResponse.result[0];
+}
--- backend/search/textSearch.js
+++ backend/search/textSearch.js
@@ -6,17 +6,17 @@ function handleRequest() {
     return {
         envs: {
             ...envs,
-            path: "/app-store-data/_search",
-            esBody: {
-                query: {
-                    multi_match: {
-                        query: requestBody.value,
-                        fields: ["Name", "Description"]
-                    }
-                },
-                _source: {
-                    exclude: ["name_vector", "desc_vector"]
+            path: "/app-store-data/_search"
+        },
+        esBody: {
+            query: {
+                multi_match: {
+                    query: requestBody.value,
+                    fields: ["Name", "Description"]
                 }
+            },
+            _source: {
+                exclude: ["name_vector", "desc_vector"]
             }
         }
     }
