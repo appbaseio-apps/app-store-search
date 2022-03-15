8e5e20de0f444d96f4de2a32e4beca275d45e5f3
Deepjyoti Barman <deep.barman30@gmail.com>
2022-03-15 09:17:56 +0530

    Add reactivesearch as a dependency

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ class App extends Component {
   render() {
     return (
       <ReactiveBase
+        url="https://appbase-demo-ansible-abxiydt-arc.searchbase.io"
         app="good-books-ds"
         credentials="nY6NNTZZ6:27b76b9f-18ea-456c-bc5e-3a5263ebc63d"
       >
