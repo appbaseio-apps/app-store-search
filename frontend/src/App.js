777eaf45b9073434cf59a071904eab440f3c19fc
Deepjyoti Barman <deep.barman30@gmail.com>
2022-03-15 09:10:00 +0530

    Fix code of app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
-import './App.css';
-import ReactDOM from "react-dom";
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-
+import React, { Component } from 'react';
+import { ReactiveBase } from '@appbaseio/reactivesearch';
+class App extends Component {
+  render() {
+    return (
+      <ReactiveBase
+        app="good-books-ds"
+        credentials="nY6NNTZZ6:27b76b9f-18ea-456c-bc5e-3a5263ebc63d"
+      >
+        Hello from Reactive Search!
+      </ReactiveBase>
+    );
+  }
+}
 export default App;
