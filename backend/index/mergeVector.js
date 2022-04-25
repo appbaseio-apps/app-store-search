7ec1a9e67312a36e711bbc098fdc75ec4f476700
Deepjyoti Barman <deep.barman30@gmail.com>
2022-04-25 12:14:01 +0530

    Add fix for merge vector stage to parse response from http

--- backend/index/mergeVector.js
+++ backend/index/mergeVector.js
@@ -1,13 +1,15 @@
 function handleRequest() {
     const requestBody = JSON.parse(context.request.body);
+    const descVector = JSON.parse(context["desc_vector"]);
+    const nameVector = JSON.parse(context["name_vector"]);
 
     return {
         request: {
             ...context.request,
             body: JSON.stringify({
                 ...requestBody,
-                "desc_vector": context["desc_vector"].data[0].embedding,
-                "name_vector": context["name_vector"].data[0].embedding
+                "desc_vector": descVector.data[0].embedding,
+                "name_vector": nameVector.data[0].embedding
             })
         }
     }
